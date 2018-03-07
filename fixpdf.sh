0641dd2236dde65d18a45a4eda88a9d3cdf76643
mahsank <mahsank@cryptolab.net>
2018-03-07 10:35:28 +0500

    Remove embedded urls from a pdf file
    A small bash script to remove embedded urls from a pdf file.

---
+++ fixpdf.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# This script tries to remove a link embedded on each page of a pdf file
+# version 0.1
+# Author: M. Ahsan
+# last updated 2017-05-27 1:55AM
+
+usage() {
+    echo "Usage: $0 [inputfile] [url]"
+}
+
+# check if the number of arguments is correct
+if [ $# -ne 2 ]
+then
+    echo "The script needs a pdf input file and url to be removed from the file."
+    usage
+    exit 1
+fi
+
+# check if qpdf and sed are installed
+CMD1="qpdf"
+CMD2="sed"
+checkutils() {
+    for j in "$CMD1" "$CMD2"; do
+        echo "checking if $j utility is available on this computer..."
+        command -v $j > /dev/null
+        if [ $? -ne 0 ]
+        then
+            echo "Command $j is not found in the path."
+            echo "Cannot continue!"
+            exit 1
+        else
+            echo "Found the $j utility in the path."
+            echo "Continuing..."
+        fi
+    done
+}
+
+FILE=$1
+URL=$2
+STAGE1="stage1"
+STAGE2="stage2"
+
+# decompress the pdf file
+decompresspdf() {
+    echo "Decompressing the pdf file $FILE."
+    if [ ! -e "$FILE" ]
+    then
+        echo "The file $FILE does not exist."
+        echo "Cannot continue!"
+        exit 1
+    else
+       "$CMD1" --stream-data=uncompress "$FILE" $STAGE1.pdf
+       if [ $? -eq 0 ]
+       then
+           echo "Successfully decompressed the pdf file."
+       else
+           echo "Failed to decompress the pdf file!"
+           exit 1
+       fi
+   fi
+}
+
+# remove the link from the decompressed pdf file
+cleanpdf() {
+    echo "Removing the url $URL from the file $FILE"
+    $CMD2 "s/$URL/ /g" <$STAGE1.pdf >$STAGE2.pdf
+    if [ $? -eq 0 ]
+    then
+        echo "Successfully removed the url from pdf file."
+    else
+        echo "Failed to remove the url from pdf file!"
+        exit 1
+    fi
+}
+
+# recompress the cleaned pdf file
+compresspdf() {
+    echo "Recompressing the pdf file."
+    "$CMD1" --stream-data=compress $STAGE2.pdf cleanedfile.pdf >& /dev/null
+}
+
+checkutils
+decompresspdf
+cleanpdf
+compresspdf
+echo "Removing intermediate files."
+rm -rf stage*.pdf
+echo
+echo "All done."
+echo
